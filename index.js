52498ed414ee769444083b174ed660a7f48637b5
lituarte <lautaro.javier.ituarte@gmail.com>
2021-11-13 22:24:58 -0300

    Refactor y agrego tests

--- index.js
+++ index.js
@@ -1,36 +1,11 @@
 require('dotenv').config()
 
-const routes = require('./routes')
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = process.env.PORT || 3000
-const swaggerJsdoc = require('swagger-jsdoc')
-const swaggerUI = require('swagger-ui-express')
+const { buildApp } = require('./app')
 const { db } = require('./db')
 
-const options = {
-  definition: {
-    openapi: '3.0.0',
-    info: {
-      title: 'API Usuarios',
-      version: '1.0.0'
-    }
-  },
-  apis: ['routes.js'] // files containing annotations
-}
+const app = buildApp(db)
 
-const openapiSpecification = swaggerJsdoc(options)
-app.use('/docs', swaggerUI.serve, swaggerUI.setup(openapiSpecification))
-
-app.use(express.json())
-
-app.use(cors())
-
-routes.setup(app, db)
-
-const server = app.listen(port, () => {
+const port = process.env.PORT || 3000
+app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-module.exports = { app, server }
--- routes.js
+++ routes.js
@@ -1,3 +1,5 @@
+const { validateEmail } = require('./utils')
+
 module.exports.setup = (app, db) => {
   /**
    * @openapi
@@ -36,21 +38,35 @@ module.exports.setup = (app, db) => {
    *          201:
    *              description: Devuelve el usuario creado
    */
-  app.post('/usuarios/add', (req, res) => {
+  app.post('/usuarios/add', async (req, res) => {
     const body = req.body
 
-    console.log(body)
+    if (!body.username || !body.password || !body.nombre || !body.apellido || typeof body.esAdmin === 'undefined') {
+      return res.status(400).json({
+        success: false,
+        error: 'Body incompleto.'
+      })
+    }
 
-    db.usuarios.add(body.username, body.password, body.nombre, body.apellido, body.esAdmin)
-      .then(function (nuevoUsuario) {
-        res.json(nuevoUsuario)
+    if (!validateEmail(body.username)) {
+      return res.status(400).json({
+        success: false,
+        error: 'El username debe ser un mail.'
       })
-      .catch(function (error) {
-        res.json({
-          success: false,
-          error: error.message || error
-        })
+    }
+
+    try {
+      const data = await db.usuarios.add(body.username, body.password, body.nombre, body.apellido, body.esAdmin)
+      res.json({
+        success: true,
+        data
       })
+    } catch (error) {
+      res.json({
+        success: false,
+        error: error.message || error
+      })
+    }
   })
 
   /**
