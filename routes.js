2049d1b666ffe38b3c960c1842be8be3b2be6292
AgustinHejeij <51308176+AgustinHejeij@users.noreply.github.com>
2021-11-30 18:40:02 -0300

    prueba

--- routes.js
+++ routes.js
@@ -388,7 +388,7 @@ module.exports.setup = (app, db) => {
       const notificador = require('node-gcm')
       const sender = notificador.Sender('AAAAzalzT_s:APA91bFtIbR8YlWZ61WG9i09D_pPz7dLUZThRykja_mp1CTqD6a6x' +
         'Rjp7O-PX4ThjgJQQDqkPX9gWw6NMsEvyop9Sf-bvmki7UXcixbLjGhRKLi8VuUv7Tckgq7d8GgUrySAd4L7oIU-')
-      const message = new notificador.Message({ notification: { title: req.body.title, body: req.body.body }, data: {} })
+      const message = new notificador.Message({ notification: { title: req.body.title, body: 'aaaa' }, data: {} })
       sender.send(message, { registrationTokens: devices }, function (err, response) {
         if (err) console.error(err)
         else console.log(response)
